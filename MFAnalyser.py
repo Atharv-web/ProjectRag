1e72a22ce4f8faa79b302b04672b5e3dc3687cd5
Atharv-web <aargangodkar@gmail.com>
2025-09-06 22:11:17 +0530

    updating the functions

--- MFAnalyser.py
+++ MFAnalyser.py
@@ -1,41 +1,14 @@
 import faiss
 from langchain_community.vectorstores import FAISS
 from langchain_ollama import OllamaEmbeddings,ChatOllama
+from langchain_core.messages import HumanMessage
 
-database_name = 'ZerodhaMidCapFunds'
+db_name = 'ZerodhaMidCapFunds'
 embedding_model = OllamaEmbeddings(model = 'nomic-embed-text:latest')
+model = ChatOllama(model="llama3.2:3b")
 
-vs = FAISS.load_local(
-    database_name,
-    embeddings=embedding_model,
-    allow_dangerous_deserialization=True
-)
-
-ret = vs.as_retriever(k=3)
-    
-# def RetrieveInfo():
-#     while True:    
-#         question = input("Enter a query:")
-#         if question.lower() == "exit":
-#             print("We are done, Thank you!!")
-#             break
-#         ans = ret.invoke(question)
-#         print(ans)
-
-# RetrieveInfo()
-
-rag_prompt = """You are an assistant for question-answering tasks.
-Here is the context to use to answer question:
-
-{context}
-
-Think carefully about the above context.
-Now, review the user question:
-
-{question}
-
-Provide an answer to this question by generating insights using the above context.
-Answer:"""
+vector_store = FAISS.load_local(db_name,embeddings=embedding_model,allow_dangerous_deserialization=True)
+retriever = vs.as_retriever(k=3)
 
 def format_docs(ans_docs):
     format_doc = []
@@ -43,10 +16,19 @@ def format_docs(ans_docs):
         format_doc.append(doc.page_content)
     return "\n".join(format_doc)
 
-model = ChatOllama(model="llama3.2:3b")
-from langchain_core.messages import HumanMessage
-
 def generate(question):
+    rag_prompt = """You are an assistant for question-answering tasks. Here is the context to use to answer question:
+
+    {context}
+
+    Think carefully about the above context.
+    Now, review the user question:
+
+    {question}
+
+    Provide an answer to this question by generating insights using the above context.
+    Answer:"""
+
     ret_doc = ret.invoke(question)
     docs_text = format_docs(ret_doc)
     rag_prompt_formatted = rag_prompt.format(context = docs_text, question  = question)
